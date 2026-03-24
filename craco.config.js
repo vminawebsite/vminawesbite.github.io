6b72cef33eafb216a513d4125a219cdfff0b0c94
vminawebsite <sites.vminawebsite@gmail.com>
2026-03-24 18:16:13 +0000

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  reactScriptsVersion: "react-scripts",
+  style: {
+    css: {
+      loaderOptions: () => {
+        return {
+          url: false,
+        };
+      },
+    },
+  },
+};
